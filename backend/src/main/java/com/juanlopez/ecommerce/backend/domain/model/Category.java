a95399aaf410d1177bd98d50134c7509a192654d
JuanDa <jlopezrodriguez95@gmail.com>
2025-11-21 01:38:01 -0500

    feat: add Category, Product, User classes and UserType enum.

---
+++ backend/src/main/java/com/juanlopez/ecommerce/backend/domain/model/Category.java
@@ -0,0 +1,15 @@
+package com.juanlopez.ecommerce.backend.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+
+    private Integer id;
+    private String name;
+
+}
---
+++ backend/src/main/java/com/juanlopez/ecommerce/backend/domain/model/UserType.java
@@ -0,0 +1,5 @@
+package com.juanlopez.ecommerce.backend.domain.model;
+
+public enum UserType {
+    ADMIN, USER
+}
