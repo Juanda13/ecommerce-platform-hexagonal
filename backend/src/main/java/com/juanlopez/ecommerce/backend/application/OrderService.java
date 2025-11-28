8ca1831befb7548652a979d7e600d007e601d2ad
JuanDa <jlopezrodriguez95@gmail.com>
2025-11-27 19:02:54 -0500

    feat(domain): add domain-level service for order management.

---
+++ backend/src/main/java/com/juanlopez/ecommerce/backend/application/OrderService.java
@@ -0,0 +1,30 @@
+package com.juanlopez.ecommerce.backend.application;
+
+import com.juanlopez.ecommerce.backend.domain.model.Order;
+import com.juanlopez.ecommerce.backend.domain.port.IOrderRepository;
+
+public class OrderService {
+
+    private final IOrderRepository iOrderRepository;
+
+    public OrderService(IOrderRepository iOrderRepository) {
+        this.iOrderRepository = iOrderRepository;
+    }
+
+    public Order save(Order order) {
+        return this.iOrderRepository.save(order);
+    }
+
+    public Iterable<Order> findAll() {
+        return this.iOrderRepository.findAll();
+    }
+
+    public Iterable<Order> fingByUserId(Integer userId) {
+        return this.iOrderRepository.findByUserId(userId);
+    }
+
+    public void updateStateById(Integer id, String state) {
+        this.iOrderRepository.updateStateById(id, state);
+    }
+
+}
